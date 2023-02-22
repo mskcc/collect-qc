368889ee07a0baea80b23934fbf7dd6b835593ae
bfarahdel <britny205@gmail.com>
2023-02-22 15:58:06 -0500

    Insert size qc metrics data for each sample can be obtained with the user's config file and qc metric data files.

---
+++ main.py
@@ -0,0 +1,24 @@
+from metric import Metric
+
+
+def process_metric(metric):
+    config_file = metric.load_config(config_file="config.yaml")
+
+    for file_specifier in config_file["metrics"]:
+        metric_functions = config_file["metrics"][file_specifier]
+        if metric_functions is None:
+            print(f"{file_specifier} is not an available metric.")
+            return
+        for operator in metric_functions["function"]:
+            operand = metric_functions["function"][operator]
+            if file_specifier == "insert_size":
+                print(metric.insert_size(operator=operator, operand=operand))
+            else:
+                print(f"{file_specifier} is not an available metric.")
+
+    return
+
+
+if __name__ == "__main__":
+    metric = Metric()
+    process_metric(metric)
---
+++ metric.py
@@ -0,0 +1,74 @@
+from io import StringIO
+import os
+import pandas as pd
+from scipy.signal import find_peaks
+import yaml
+
+
+class Metric:
+    def __init__(self):
+        self.config = None
+        self.qc_data_path = None
+
+    def load_config(self, config_file):
+        with open(config_file) as f:
+            config = yaml.safe_load(f)
+        self.qc_data_path = config["qc_data_path"]
+        return config
+
+    def insert_size(self, operator, operand):
+        runs = os.listdir(self.qc_data_path)
+        insert_size_data = []
+        for run in runs:
+            run_path = os.path.join(self.qc_data_path, run)
+            for run_file in os.listdir(run_path):
+                if run_file.endswith(".ismetrics"):
+                    with open(os.path.join(run_path, run_file)) as f:
+                        hist_flag = False
+                        hist = []
+                        for line in f:
+                            if line.startswith("## HISTOGRAM"):
+                                hist_flag = True
+                            elif hist_flag:
+                                hist.append(line)
+
+                        hist_data = pd.read_csv(StringIO("\n".join(hist)), sep="\t")
+                        fun = Function(operator, operand)
+                        tab_data = fun(hist_data)
+                        if len(tab_data.columns) < 2:
+                            return f"Incorrect columns specified in the config file."
+                        peak_data = tab_data.iloc[:, 1]
+                        max_peak_idx = peak_data.idxmax()
+                        max_peak = tab_data.iloc[:, 0].iloc[max_peak_idx]
+                        peaks = find_peaks(
+                            peak_data, height=max_peak / 3, distance=10, width=10
+                        )
+                        insert_size_data.append(
+                            f'{run_file.split(".")[0]}, peaks: {len(peaks[0])}, max_peak: {max_peak}'
+                        )
+                        insert_size_out = "\n".join(insert_size_data)
+
+        return insert_size_out
+
+
+class Function:
+    def __init__(self, operator, operand):
+        self.operator = operator
+        self.operand = operand
+
+    def __call__(self, data):
+        if self.operator == "table":
+            return self.table(data)
+        else:
+            return f"{self.operator} is not an available function."
+
+    def table(self, data):
+        cols = []
+        # Convert from 1-based to 0-based indexing
+        if self.operand["columns"]:
+            for i in range(0, len(self.operand["columns"])):
+                cols.append(self.operand["columns"][i] - 1)
+
+            return data.iloc[:, cols]
+        else:
+            return data
